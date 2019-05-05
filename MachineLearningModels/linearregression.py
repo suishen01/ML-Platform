dd77ec37516b298684388a68013cff018fcc4d90
Jeremy <xyan510@outlook.com>
2019-05-05 14:20:45 +0800

    Added linear regression and corresponding test

---
+++ MachineLearningModels/linearregression.py
@@ -0,0 +1,54 @@
+from MachineLearningModels.model import Model
+from sklearn.linear_model import LinearRegression as LinearRegressionModel
+
+class LinearRegression(Model):
+
+    # X represents the features, Y represents the labels
+    X = None
+    Y = None
+    prediction = None
+    model = None
+
+
+    def __init__(self, X=None, Y=None):
+
+        if X is not None:
+            self.X = X
+
+        if Y is not None:
+            self.Y = Y
+
+        self.model = LinearRegressionModel()
+
+
+    def fit(self, X=None, Y=None):
+        if X is not None:
+            self.X = X
+
+        if Y is not None:
+            self.Y = Y
+
+        print('Linear Regression Train started............')
+        self.model.fit(self.X, self.Y)
+        print('Linear Regression completed..........')
+
+        return self.model
+
+    def predict(self, test_features):
+        print('Prediction started............')
+        self.predictions = self.model.predict(test_features)
+        print('Prediction completed..........')
+        return self.predictions
+
+
+    def save(self):
+        print('No models will be saved for lasso')
+
+    def featureImportance(self, X_headers=None):
+        if X_headers is None:
+            X_headers = list(self.X)
+        print(self.model.coef_)
+        feature_importance_ = zip(self.model.coef_[0], X_headers)
+        feature_importance = set(feature_importance_)
+
+        return feature_importance
---
+++ linearregression_test.py
@@ -0,0 +1,36 @@
+import numpy as np
+from sklearn.metrics import r2_score
+from MachineLearningModels.linearregression import LinearRegression
+from Utils.csvread import CsvReader
+from DataPreprocessor.datacleaner import Cleaner
+from DataPreprocessor.dataspliter import Spliter
+from Evaluation.evaluation import Evaluation
+
+csvreader = CsvReader()
+data = csvreader.read('data_small_withLaggedOutputs.csv')
+
+labels = data[['ret']].copy()
+features = data[['dp','epm','bmm','ntis','tbl','tms','dfy','svar','bm','ep','maxret','absacc','acc','aeavol','age','agr','baspread','beta','betasq','bm_ia','cash','cashdebt','cashpr','cfp','cfp_ia','chatoia','chcsho','chempia','chinv','chmom','chpmia','chtx','cinvest','convind','currat','depr','divi','divo','dolvol','dy','ear','egr','gma','grCAPX','grltnoa','herf','hire','idiovol','ill','indmom','invest','lev','lgr','mom12m','mom1m','mom36m','mom6m','ms','mvel1','mve_ia','nincr','operprof','orgcap','pchcapx_ia','pchcurrat','pchdepr','pchgm_pchsale','pchsale_pchxsga','pchsaleinv','pctacc','pricedelay','ps','quick','rd','rd_mve','rd_sale','realestate','retvol','roaq','roavol','roeq','roic','rsup','salecash','saleinv','salerec','secured','securedind','sgr','sin','sp','std_dolvol','std_turn','stdacc','stdcf','tang','tb','turn','zerotrade','ret_lagged','retrf_lagged','maxret_lagged']].copy()
+
+cleaner1 = Cleaner(labels)
+cleaner2 = Cleaner(features)
+labels = cleaner1.clean()
+labels = cleaner1.data
+features = cleaner2.clean()
+features = cleaner2.data
+
+spliter = Spliter()
+# Split the data into training and testing sets
+train_features, test_features, train_labels, test_labels = spliter.split(features, labels, test_size = 0.25, random_state = 42)
+
+print('Training Features Shape:', train_features.shape)
+print('Training Labels Shape:', train_labels.shape)
+print('Testing Features Shape:', test_features.shape)
+print('Testing Labels Shape:', test_labels.shape)
+
+lr = LinearRegression()
+
+lr.fit(train_features, train_labels)
+y_pred_lr = lr.predict(test_features)
+print(lr.featureImportance())
+print(r2_score(test_labels, y_pred_lr))
